03b3ad8e169d396d1ade407bfa9b1e493fbde583
stuffegit <chrsundstrom@gmail.com>
2024-09-23 22:05:54 +0200

    neotree, neoline, treesitter, lsp for c/cpp/py/lua, theme, telescope

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,8 +1,8 @@
-return { 
-  "nvim-neo-tree/neo-tree.nvim", 
-  branch = "v3.x", 
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
   dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim", "3rd/image.nvim", },
-  config = function() 
+  config = function()
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {} )
   end
 }
